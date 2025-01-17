6dfebc99288ea8b5b57c90a41c1e279de4c0abf1
yohannd1 <mitonanan12@gmail.com>
2025-01-16 21:53:49 -0300

    adicionando código básico para uso do buzzer (buzzer.c)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 #include "pico/stdlib.h"
+#include "buzzer.h"
+
+#define BUZZER_PIN 10
 
 int main(void) {
 	stdio_init_all();
+
+	Buzzer bz;
+	Buzzer_init(&bz, BUZZER_PIN);
+	Buzzer_play(&bz, 2090, 500); // isso aqui toca o buzzer por 500ms. TODO:
+								 // conectar isso ao teclado
 
 	for (int i = 0;; i++) {
 		printf("Hello %d\n", i);
