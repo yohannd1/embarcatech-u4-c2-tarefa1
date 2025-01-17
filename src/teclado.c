6f5e3e3603f034db39575c7135581a6010d4f6ca
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-17 18:07:26 -0300

    Implementando a função 'obter_tecla_pressionada'
    Realiza a varredua habilitando as linhas e verificando se alguma coluna
    foi ativada.
    
    Retorna o caractere pressionado e desliga a linha que está sendo
    verificada.
    
    Caso não haja tecla pressionada, retorna '\0'.

--- src/teclado.c
+++ src/teclado.c
@@ -15,3 +15,30 @@ void inicializar_teclado(uint pinos_colunas[],uint pinos_linhas[], int tamanho_a
         gpio_put(pinos_linhas[contador],false);
 	}
 }
+
+char obter_tecla_pressionada(uint pinos_colunas[],uint pinos_linhas[], int tamanho_array_colunas, int tamanho_array_linhas, char mapa_caracteres[4][4]){
+	bool tecla_pressionada = false;
+	
+	// Realiza a varredura das linhas
+	for (int contador_linhas = 0; contador_linhas < tamanho_array_linhas; contador_linhas++){
+		// Coloca atual no nível alto
+		gpio_put(pinos_linhas[contador_linhas],true);
+
+		// Realiza a varredura das colunas
+		for (int contador_colunas = 0; contador_colunas < tamanho_array_colunas; contador_colunas++)
+		{
+			// Verifica se o botão foi pressionado
+			if (gpio_get(pinos_colunas[contador_colunas])){
+				// Desliga a linha atual para evitar conflitos caso mantenha a tecla pressionada
+				gpio_put(pinos_linhas[contador_linhas],false);
+
+				return mapa_caracteres[contador_linhas][contador_colunas];			
+			}
+		}
+
+		// Coloca a linha atual no nível baixo
+		gpio_put(pinos_linhas[contador_linhas],false);
+	}
+
+	return '\0';
+}
