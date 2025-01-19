b48efe890b985ba90cfab4c55f28e2ad416c87f1
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-19 10:15:21 -0300

    Removendo comentários do arquivo led.c

--- src/led.c
+++ src/led.c
@@ -17,8 +17,4 @@ void set_led_color(bool red, bool green, bool blue) {
 	gpio_put(LED_RED_PIN, red);
 	gpio_put(LED_GREEN_PIN, green);
 	gpio_put(LED_BLUE_PIN, blue);
-
-	/* printf("LED_RED_PIN=%d\n", red); */
-	/* printf("LED_GREEN_PIN=%d\n", green); */
-	/* printf("LED_BLUE_PIN_PIN=%d\n", blue); */
 }
